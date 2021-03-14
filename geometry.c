3395a21bc9b5a4c13a75643826cbfd44fe5da0bb
cicerotcv <elm.tiago@gmail.com>
2021-03-14 18:32:45 -0300

    case 2

--- geometry.c
+++ geometry.c
@@ -1,11 +1,14 @@
-#include <math.h>
-
 #include "geometry.h"
 
+#include <math.h>
+
+#include "helpers.c"
+
 int verify(point p, point a, point b) {
+    if (case_2(p, a, b)) {
+        return 2;
+    }
     return 0;
 }
 
-int inside(point p, point poly[], int n) {
-    return 0;
-}
+int inside(point p, point poly[], int n) { return 0; }
