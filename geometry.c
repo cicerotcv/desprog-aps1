0d9f30cfad7fad2a91b528b9fd38bd7ad07ad8a2
cicerotcv <elm.tiago@gmail.com>
2021-03-20 21:47:57 -0300

    all passing

--- geometry.c
+++ geometry.c
@@ -9,11 +9,21 @@
 int is_equal(double a, double b) { return abs(a - b) < threshold; }
 int points_equal(point a, point b) { return a.x == b.x && a.y == b.y; }
 
+/**
+ * calcula a proporção entre os segmentos AP e AB
+ */
 double calc_lambda(point p, point a, point b) {
-    if (a.x != b.x) {
-        return (p.x - a.x) / (b.x - a.x);
+    int delta_x_ab = b.x - a.x;
+    int delta_y_ab = b.y - a.y;
+    int delta_x_ap = p.x - a.x;
+    int delta_y_ap = p.y - a.y;
+
+    if (delta_x_ab != 0 && delta_x_ap != 0) {
+        return delta_x_ap / (double)delta_x_ab;
+    } else if (delta_y_ab != 0 && delta_y_ap != 0) {
+        return delta_y_ap / (double)delta_y_ab;
     }
-    return (p.y - a.y) / (b.y - a.y);
+    return 0;
 }
 
 int is_between(double number, double a, double b) {
@@ -36,8 +46,8 @@ int case_2(point p, point a, point b) {
         return 1;
     }
     double lambda = calc_lambda(p, a, b);
-    return (p.y - a.y) * (a.x - b.x) == (p.x - a.x) * (a.y - b.y) &&
-           is_between(lambda, 0.0, 1.0);
+    int result = (p.y - a.y) * (a.x - b.x) == (p.x - a.x) * (a.y - b.y);
+    return result && is_between(lambda, 0.0, 1.0);
 }
 
 int case_1(point p, point a, point b) {
@@ -72,5 +82,18 @@ int verify(point p, point a, point b) {
 }
 
 int inside(point p, point poly[], int n) {
-    return 0;
+    int crossings = 0;
+
+    for (int i = 0; i < n; i++) {
+        point a = poly[i];
+        point b = i == n - 1 ? poly[0] : poly[i + 1];
+
+        int result = verify(p, a, b);
+        if (result == 2) {
+            return 1;
+        } else if (result == 1) {
+            crossings++;
+        }
+    }
+    return crossings % 2;
 }
